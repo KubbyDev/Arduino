d93c2eef86d4ffaede035243e1787d6968f4ecf9
Gabriel <gbrljorge@gmail.com>
2019-06-09 20:54:40 +0200

    Custom Controller (For Unity Game)

---
+++ UnityController/ArduinoControlledGame/Assets/Character.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 10;
+    
+    void Update()
+    {
+        Inputs.ReadFromArduino();
+        
+        transform.position +=
+            (Inputs.forward * transform.forward + Inputs.right * transform.right)
+            * Time.deltaTime * movementSpeed;
+    }
+}
