9240a7048aaa3fe3fcd78a5dc3c8f969ffab9dd4
KubbyDev <gbrljorge@gmail.com>
2019-06-12 11:39:59 +0200

    Clean up + Tentative ratee de requette depuis Android Studio

--- LedSwitch/AndroidApp/app/src/main/java/com/example/arduinocontroller/MainActivity.java
+++ LedSwitch/AndroidApp/app/src/main/java/com/example/arduinocontroller/MainActivity.java
@@ -1,7 +1,21 @@
 package com.example.arduinocontroller;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +24,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selectStateOn(View view) {
+        sendHTTPRequest("?b=3");
+    }
+
+    public void selectStateOff(View view) {
+        sendHTTPRequest("?b=");
+    }
+
+    private void sendHTTPRequest(String arguments) {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://localhost:4000/" + arguments;
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error.getMessage());
+                    }
+                }
+        );
+
+        System.out.println("Sending Http Request");
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    /*
+    private void sendHTTPRequest(final String arguments) {
+
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground( final Void ... params ) {
+
+                try {
+
+                    //Init
+                    //URL url = new URL("http://192.168.0.136:4200/");
+                    URL url = new URL("http://localhost:4000/");
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+
+                    //Parametres
+                    connection.setDoOutput(true);
+                    DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                    out.writeBytes(arguments);
+                    out.flush();
+                    out.close();
+
+                    //Envoi
+                    connection.setRequestProperty("Content-Type", "application/json");
+
+                } catch (Exception e) { e.printStackTrace(); }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute( final Void result ) {
+            }
+
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null);
+    }
+    */
 }
--- LedSwitch/WebPage/Script.js
+++ LedSwitch/WebPage/Script.js
@@ -1,35 +1,23 @@
 let ledPin = [];
-let adresse = "http://" + document.getElementById("ip").value + ":" + document.getElementById("port").value + "/";
 
-document.addEventListener('DOMContentLoaded', setup, false);
+ledPin = document.getElementById("broche3");
+document.getElementById("envoyer").addEventListener('click', send, false);
 
-function setup() {
+function send() {
 
-    // fonction qui va lier les variables à leur conteneur HTML
-    ledPin = document.getElementById("broche3");
-
-    // La fonction concernant le bouton
-    let bouton = document.getElementById("envoyer");
-    bouton.addEventListener('click', executer, false);
-}
-
-function executer() {
-
-    // Fonction qui va créer l'url avec les paramètres puis
-    // envoyer la requête
-    let requete = new XMLHttpRequest(); // créer un objet de requête
-    let url = adresse;
+    let request = new XMLHttpRequest();
+    let url = "http://" + document.getElementById("ip").value + ":" + document.getElementById("port").value + "/";
     url += "?b=";
 
     if(ledPin.checked)
         url += "3,";
 
-    // enlève la dernière virgule si elle existe
+    // enleve la derniere virgule si elle existe
     if(url[url.length-1] === ',')
         url = url.substring(0, url.length-1);
 
-    console.log(url); // Pour debugguer l'url formée
+    console.log(url);
 
-    requete.open("GET", url, true); // On construit la requête
-    requete.send(null); // On envoie !
+    request.open("GET", url, true);
+    request.send(null);
 }
